f7b1511c2c1a4e0cc35125539e56f330e8ca1996
Secrets <zmzxdal@foxmail.com>
2023-12-28 16:28:14 +0800

    FEAT:pro-init

---
+++ chatgpt-data-app/src/main/java/com/zmz/chatgpt/data/ChatgptDataApplication.java
@@ -0,0 +1,13 @@
+package com.zmz.chatgpt.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptDataApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ChatgptDataApplication.class);
+    }
+
+}

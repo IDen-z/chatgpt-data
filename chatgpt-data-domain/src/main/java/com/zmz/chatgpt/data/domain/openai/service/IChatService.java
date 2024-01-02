72ecababa9d95f80ea251b6cd6cab5f0722189d0
Secrets <zmzxdal@foxmail.com>
2024-01-02 16:36:46 +0800

    FEAT:对接chatgpt-sdk

---
+++ chatgpt-data-domain/src/main/java/com/zmz/chatgpt/data/domain/openai/service/IChatService.java
@@ -0,0 +1,10 @@
+package com.zmz.chatgpt.data.domain.openai.service;
+
+import com.zmz.chatgpt.data.domain.openai.model.aggregates.ChatProcessAggregate;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+
+public interface IChatService {
+
+    ResponseBodyEmitter completions(ChatProcessAggregate chatProcess);
+
+}
---
+++ chatgpt-data-trigger/src/main/java/com/zmz/chatgpt/data/trigger/http/dto/ChatGPTRequestDTO.java
@@ -0,0 +1,23 @@
+package com.zmz.chatgpt.data.trigger.http.dto;
+
+import com.zmz.chatgpt.data.types.enums.ChatGPTModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatGPTRequestDTO {
+
+    /** 默认模型 */
+    private String model = ChatGPTModel.GPT_3_5_TURBO.getCode();
+
+    /** 问题描述 */
+    private List<MessageEntity> messages;
+
+}
